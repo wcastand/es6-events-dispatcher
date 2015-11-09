973713c9fb6d9c59e13797c6b62c9add564f5d3a
wcastand <wcastand@gmail.com>
2015-11-09 19:02:32 +0100

    te

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ export class Dispatcher{
   constructor(){
     this.events = []
   }
-  on(scope, name, callback){
-    if(!this.events[name]){
+  on(name, callback, scope = this){
+    if(!this.events[name])
       this.events[name] = []
     this.events[name].push({scope, callback})
   }
@@ -12,9 +12,9 @@ export class Dispatcher{
       this.events[name].filter(e => e.scope != scope)
     }
   }
-  fire(name, data, arrayOfData = false){
+  fire(name, data){
     this.events[name].forEach(e => {
-      e.callback.call(e.scope, ...data)
+      e.callback.call(e.scope, data)
     })
   }
 }
